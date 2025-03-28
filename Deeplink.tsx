fe87e106f0a9d790b4972e44d7241d78feefa2f2
Fattah Muhyiddeen <fattahmuhyiddeen@gmail.com>
2025-03-28 18:59:26 +0800

    persist deeplink

--- Deeplink.tsx
+++ Deeplink.tsx
@@ -1,28 +1,66 @@
-import React from 'react';
-import { Linking, Text, TextInput, TouchableOpacity, View } from 'react-native';
+import React, { useEffect } from "react";
+import { Linking, Text, TextInput, TouchableOpacity, View } from "react-native";
 
-export default ({deeplinkPrefix, onClose}) => {
-  const [text, setText] = React.useState('');
+let LocalStorage;
+try {
+  LocalStorage =
+    require("@react-native-async-storage/async-storage/src").default;
+} catch (error) {
+  // console.error("Error importing LocalStorage:", error);
+}
+
+export default ({ deeplinkPrefix, onClose }) => {
+  const [text, setText] = React.useState("");
+  const [history, setHistory] = React.useState([]);
+
+  const go = (t) => {
+    const newHistory = [t, ...history.filter((h) => h !== t)].slice(0, 3);
+    setHistory(newHistory);
+    LocalStorage?.setItem(
+      "in-app-debugger-deeplink-list",
+      JSON.stringify(newHistory)
+    );
+    onClose();
+    Linking.openURL(deeplinkPrefix + t);
+  };
+
+  useEffect(() => {
+    LocalStorage?.getItem("in-app-debugger-deeplink-list").then((d) => {
+      if (d) {
+        setHistory(JSON.parse(d));
+      }
+    });
+  }, []);
   return (
-    <View style={{ flexDirection: 'row', alignItems: 'center', margin: 10 }}>
-      <Text style={{ color: 'white' }}>{deeplinkPrefix}</Text>
-      <TextInput
-        value={text}
-        autoCapitalize='none'
-        autoCorrect={false}
-        autoComplete='off'
-        onChangeText={setText}
-        style={{ flex: 1, backgroundColor: '#555', marginRight: 10, padding: 3, borderRadius: 3 }}
-      />
-      <TouchableOpacity
-        style={{ padding: 7, borderRadius: 4, backgroundColor: 'white' }}
-        onPress={() => {
-          onClose();
-          Linking.openURL(deeplinkPrefix + text);
-        }}
-      >
-        <Text style={{ color: 'black', fontSize: 9 }}>Go</Text>
-      </TouchableOpacity>
+    <View style={{ marginBottom: 10 }}>
+      <View style={{ flexDirection: "row", alignItems: "center", margin: 10 }}>
+        <Text style={{ color: "white" }}>{deeplinkPrefix}</Text>
+        <TextInput
+          value={text}
+          autoCapitalize="none"
+          autoCorrect={false}
+          autoComplete="off"
+          onChangeText={setText}
+          style={{
+            flex: 1,
+            backgroundColor: "#555",
+            marginRight: 10,
+            padding: 3,
+            borderRadius: 3,
+          }}
+        />
+        <TouchableOpacity
+          style={{ padding: 7, borderRadius: 4, backgroundColor: "white" }}
+          onPress={() => go(text)}
+        >
+          <Text style={{ color: "black", fontSize: 9 }}>Go</Text>
+        </TouchableOpacity>
+      </View>
+      {history.map((h) => (
+        <TouchableOpacity style={{ margin: 6 }} onPress={() => go(h)}>
+          <Text style={{ color: "grey", textAlign: "right" }}>{h}</Text>
+        </TouchableOpacity>
+      ))}
     </View>
   );
 };
