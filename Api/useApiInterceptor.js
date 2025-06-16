b0b67e1ed7a5c6bd3edf351e84d79c130dfdb805
Fattah Muhyiddeen <fattahmuhyiddeen@gmail.com>
2025-06-16 10:57:17 +0800

    log fetch body

--- Api/useApiInterceptor.js
+++ Api/useApiInterceptor.js
@@ -1,16 +1,16 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState } from "react";
 // import XHRInterceptor from 'react-native/Libraries/Network/XHRInterceptor.js';
-import axios from 'axios';
+import axios from "axios";
 // const overrideResponse = false; // Set to true to override the response with a random value
 // const filterNonBusinessRelatedAPI = true;
 
 const originalFetch = global.fetch;
 
 const shouldExclude = (url, method) =>
-  ['HEAD'].includes(method) ||
-  url.includes('codepush') ||
-  url.includes('localhost') ||
-  url.includes('applicationinsights.azure.com');
+  ["HEAD"].includes(method) ||
+  url.includes("codepush") ||
+  url.includes("localhost") ||
+  url.includes("applicationinsights.azure.com");
 
 const parse = (data) => {
   try {
@@ -38,11 +38,16 @@ export default ({ maxNumOfApiToStore, blacklists, blacklistRef, mocks }) => {
   }, [apis]);
 
   const makeRequest = (data) => {
-    if (blacklistRef.current.some((b) => b.url === data.url && b.method === data.method)) return;
+    if (
+      blacklistRef.current.some(
+        (b) => b.url === data.url && b.method === data.method
+      )
+    )
+      return;
     const date = new Date();
     let hour = date.getHours();
-    const minute = (date.getMinutes() + '').padStart(2, '0');
-    const second = (date.getSeconds() + '').padStart(2, '0');
+    const minute = (date.getMinutes() + "").padStart(2, "0");
+    const second = (date.getSeconds() + "").padStart(2, "0");
 
     const request = {
       ...data,
@@ -67,7 +72,14 @@ export default ({ maxNumOfApiToStore, blacklists, blacklistRef, mocks }) => {
   };
 
   useEffect(() => {
-    setApis((v) => v.filter((v) => !blacklists.some((b) => b.url === v.request.url && b.method === v.request.method)));
+    setApis((v) =>
+      v.filter(
+        (v) =>
+          !blacklists.some(
+            (b) => b.url === v.request.url && b.method === v.request.method
+          )
+      )
+    );
   }, [blacklists]);
 
   const receiveResponse = (data) => {
@@ -77,7 +89,12 @@ export default ({ maxNumOfApiToStore, blacklists, blacklistRef, mocks }) => {
       const oldData = [...v];
       for (let i = 0; i < oldData.length; i++) {
         const old = oldData[i];
-        if (old.response || old.request.url !== data.config.url || old.request.method !== data.config.method) continue;
+        if (
+          old.response ||
+          old.request.url !== data.config.url ||
+          old.request.method !== data.config.method
+        )
+          continue;
 
         oldData[i].response = {
           ...data,
@@ -94,13 +111,16 @@ export default ({ maxNumOfApiToStore, blacklists, blacklistRef, mocks }) => {
   useEffect(() => {
     interceptorIds.forEach((id) => axios.interceptors.request.eject(id));
     interceptorIds = [];
-    const mocked = (url, method) => mocks.find(
+    const mocked = (url, method) =>
+      mocks.find(
         (m) =>
-          m.request.url.toLowerCase() === url.toLowerCase() && m.request.method.toLowerCase() === method.toLowerCase() && m.active,
+          m.request.url.toLowerCase() === url.toLowerCase() &&
+          m.request.method.toLowerCase() === method.toLowerCase() &&
+          m.active
       );
     global.fetch = async function (...args) {
       // console.log('xxxxxxxx [Fetch Request]', args);
-      const [url, { headers, method, body } = { method: 'get' }] = args;
+      const [url, { headers, method, body } = { method: "get" }] = args;
 
       try {
         if (!shouldExclude(url, method)) {
@@ -111,7 +131,7 @@ export default ({ maxNumOfApiToStore, blacklists, blacklistRef, mocks }) => {
             headers,
             method,
             data,
-            interface: 'fetch',
+            interface: "fetch",
           });
         }
 
@@ -122,28 +142,16 @@ export default ({ maxNumOfApiToStore, blacklists, blacklistRef, mocks }) => {
 
         const response = await originalFetch(...args);
         // console.log('xxxxxx [Fetch Response]', response);
-        // const data = await response.json();
-        // alert('aaaa')
-        // const returnedTarget = Object.assign({}, response);
-        // const data = await returnedTarget.json();
 
-        // const reader = response.body.getReader();
-        // const decoder = new TextDecoder();
-        // let result = '';
+        let data = "";
 
-        // function read() {
-        //   return reader.read().then(({ done, value }) => {
-        //     if (done) {
-        //       console.log('Response body:', result);
-        //       return;
-        //     }
-        //     result += decoder.decode(value, { stream: true });
-        //     return read();
-        //   });
-        // }
-
-        // const data = read();
-        // alert(JSON.stringify(data));
+        if (response.ok) {
+          const responseClone = response.clone();
+          data = await responseClone.text();
+          try {
+            data = JSON.parse(data);
+          } catch (e) {}
+        }
 
         receiveResponse({
           config: {
@@ -152,7 +160,7 @@ export default ({ maxNumOfApiToStore, blacklists, blacklistRef, mocks }) => {
             method: response.method || method,
           },
           status: response.status,
-          // data: { aaa: 'bbb' },
+          data,
         });
 
         // console.log('xxxxxx fetch checking mock');
@@ -178,82 +186,86 @@ export default ({ maxNumOfApiToStore, blacklists, blacklistRef, mocks }) => {
 
         return response;
       } catch (error) {
-        // console.log(error);
+        console.log("xxxxxxxxx error", error);
         // console.error('[Fetch Error]', error);
         return new Response(JSON.stringify(error), {
           status: 400,
-          headers: { 'Content-Type': 'application/json' },
+          headers: { "Content-Type": "application/json" },
         });
         // throw error;
       }
     };
 
     // --- Intercept Axios globally ---
-    interceptorIds.push(axios.interceptors.request.use(
-      (config) => {
-        // console.log('[Axios Request]', config);
+    interceptorIds.push(
+      axios.interceptors.request.use(
+        (config) => {
+          // console.log('[Axios Request]', config);
 
-        const { method, url, headers } = config;
-        if (!shouldExclude(url, method)) {
-          const data = config.data ? parse(config.data) : undefined;
+          const { method, url, headers } = config;
+          if (!shouldExclude(url, method)) {
+            const data = config.data ? parse(config.data) : undefined;
 
-          const mock = mocked(url, method);
+            const mock = mocked(url, method);
 
-          makeRequest({
-            url,
-            headers,
-            method,
-            data,
-            mockid: mock?.id,
-            interface: 'axios',
+            makeRequest({
+              url,
+              headers,
+              method,
+              data,
+              mockid: mock?.id,
+              interface: "axios",
+            });
+          }
+
+          return config;
+        },
+        (error) => {
+          // alert(JSON.stringify(error));
+          // console.error('[Axios Request Error]', error);
+
+          receiveResponse({
+            config: error.config,
+            status: error.response?.status,
           });
+          return Promise.reject(error);
         }
+      )
+    );
 
-        return config;
-      },
-      (error) => {
-        // alert(JSON.stringify(error));
-        // console.error('[Axios Request Error]', error);
+    interceptorIds.push(
+      axios.interceptors.response.use(
+        (response) => {
+          // console.log('[Axios Response]', response);
 
-        receiveResponse({
-          config: error.config,
-          status: error.response?.status,
-        });
-        return Promise.reject(error);
-      },
-    ));
+          // if (overrideResponse) {
+          //   // Replace response.data with random value
+          // response.data = { message: 'Random overridden Axios response' };
+          // response.status = 200;
+          // }
+          // mocked(response.config.url, response.config.method)?.response?.data
 
-    interceptorIds.push(axios.interceptors.response.use(
-      (response) => {
-        // console.log('[Axios Response]', response);
+          // console.log('xxxxxx axios checking mock');
+          const mock = mocked(response.config.url, response.config.method);
 
-        // if (overrideResponse) {
-        //   // Replace response.data with random value
-        // response.data = { message: 'Random overridden Axios response' };
-        // response.status = 200;
-        // }
-        // mocked(response.config.url, response.config.method)?.response?.data
+          if (mock?.response) {
+            // console.log('xxxxxx axios mocked', mock);
+            response = mock.response;
+          }
+          // console.log('xxxxxx axios response', response);
+          receiveResponse(response);
 
-        // console.log('xxxxxx axios checking mock');
-        const mock = mocked(response.config.url, response.config.method);
-
-        if (mock?.response) {
-          // console.log('xxxxxx axios mocked', mock);
-          response = mock.response;
+          return response;
+        },
+        (error) => {
+          receiveResponse({
+            config: error.config,
+            status: error.response?.status,
+          });
+          return Promise.reject(error);
         }
-        // console.log('xxxxxx axios response', response);
-        receiveResponse(response);
-
-        return response;
-      },
-      (error) => {
-        receiveResponse({
-          config: error.config,
-          status: error.response?.status,
-        });
-        return Promise.reject(error);
-      },
-    ));
+      )
+    );
   }, [mocks]);
 
   const deleteApi = (id) => {
